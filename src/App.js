5f90fafd0f55332bef5ab726b30e244ee0a4b2a8
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-04-14 14:46:13 +0300

    add add task functional

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import { useState } from 'react';
 
 function App() {
-  return <></>;
+  const [tasks, setTasks] = useState([]);
+  const [value, setValue] = useState('');
+
+  console.log(value);
+
+  const addTask = () => {
+    setTasks([
+      ...tasks,
+      {
+        id: 1,
+        title: value,
+      },
+    ]);
+  };
+
+  console.log(tasks);
+  return (
+    <>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={addTask}>Add</button>
+      <button>Delete</button>
+      <button>Edit</button>
+      <div>
+        {tasks.map((items) => (
+          <li>{items.title}</li>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
