5b5e799f96106c3ae45e493e72b536fff084718f
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-04-17 13:00:05 +0300

    add readme file & change title to reactive todo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -64,7 +64,7 @@ export default function Header({ setSearchValue }) {
               textAlign: 'center',
             }}
           >
-            React Todo State
+            Reactive Todo
           </Typography>
           <Search>
             <SearchIconWrapper>
