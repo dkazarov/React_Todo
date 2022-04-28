82f1c84362859684c54e0277059e8fb165714f03
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-04-28 11:42:44 +0300

    add firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCVhAK7ugBx8Lu2KpOs1y8QOxKd8CB2PlE',
+  authDomain: 'todo-2ed09.firebaseapp.com',
+  databaseURL:
+    'https://todo-2ed09-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'todo-2ed09',
+  storageBucket: 'todo-2ed09.appspot.com',
+  messagingSenderId: '727384853001',
+  appId: '1:727384853001:web:8ba5d47093ca3a66d846d8',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
